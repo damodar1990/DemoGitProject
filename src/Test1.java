aef27b40e9ff9add82ff71a56dc419dc3627853e
Shubha <Shubha@DESKTOP-2GNJ49D>
2022-04-13 02:05:28 -0700

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat");
+		}
+		else
+		{
+			System.out.println("sleep");
+		}
+		
+	}
+
+}
